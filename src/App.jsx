c0e7d3c1c40755460794f7fb05bdda706c349f75
junaidahmedjs1 <Junaidahmed31254@gmail.com>
2025-03-04 16:49:42 +0500

    Todo App with React

--- src/App.jsx
+++ src/App.jsx
@@ -41,25 +41,18 @@ function App(){
     <h1>TODO APP</h1>
     <form onSubmit={addTodo}>
       <input type="text" placeholder="Tilte" ref={title} />
-      <br /><br />
       <input type="text" placeholder="Description" ref={description} />
-      <br /><br />
       <button>Add TODO</button>
     </form>
     <div>
     {todo.length > 0 ? todo.map((item , index)=>{
-        return <div key={item.Id} style={{
-          border: '1px solid black',
-          borderRadius: '20px',
-          padding: '30px',
-          margin: '40px'
-        }}>
-          <h1>title: {item.title}</h1>
-          <h1>desc: {item.description}</h1><br />
-          <button onClick={()=>editTodo(index)}  style={{margin:`20px`}}>edit</button>
+        return <div key={item.Id}>
+          <h1>Title: {item.title}</h1>
+          <h1>Desc: {item.description}</h1><br />
+          <button onClick={()=>editTodo(index)}>edit</button>
           <button onClick={()=>deleteTodo(index)}>Delete</button>
         </div>
-      }): <h1>No todos Found</h1>}
+      }): <h1>No todos Found.....</h1>}
       </div>
       </>
   )
